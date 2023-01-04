7fc3faf90149944c5ac2e6e15ad2935b921c614c
Yosra DIRGHAM <32835818+yosradirgham@users.noreply.github.com>
2023-01-04 07:08:23 +0100

    Update update_asset.py
    Fix action update_asset

--- sekoiaio/update_asset.py
+++ sekoiaio/update_asset.py
@@ -8,18 +8,22 @@ def update_asset(config, params):
     """
     Update a specific asset
     """
-
+    
     url: str = f"{ASSETS_BASE_URL}/{params['asset_uuid']}"
+    attributes: list = list(params.get("asset_attributes").split(",")) if params.get("asset_attributes") else []
+    keys: list = list(params.get("asset_keys").split(",")) if params.get("asset_keys") else []
+    owners: list = list(params.get("asset_owners").split(",")) if params.get("asset_owners") else []
+    
     payload: dict = {
-        "asset_type": params["asset_type"],
-        "name": params.get("name", None),
-        "description": params.get("description", None),
-        "criticity": params.get("criticity", None),
-        "attributes": params.get("attributes", None),
-        "keys": params.get("keys", None),
-        "owners": params.get("owners", None),
+        "asset_type": {"uuid": params["asset_type_uuid"], "name": params["asset_type_name"]},
+        "name": params.get("asset_name", None),
+        "description": params.get("asset_description") or "",
+        "criticity": params.get("asset_criticity", None),
+        "attributes": attributes,
+        "keys": keys,
+        "owners": owners,
     }
-
+    
     try:
         response = GenericAPIAction(config, "PUT", url, json=payload).run()
     except Exception as e:
