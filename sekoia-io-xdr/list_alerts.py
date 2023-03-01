e96fb24de692a335ad53d123cf8320d1c154ed83
Yosra DIRGHAM <32835818+yosradirgham@users.noreply.github.com>
2023-03-01 10:16:09 +0100

    Update list_alerts.py

--- sekoia-io-xdr/list_alerts.py
+++ sekoia-io-xdr/list_alerts.py
@@ -8,13 +8,20 @@ logger = get_logger("sekoia-io-xdr")
 
 def list_alerts(config, params):
     url: str = OPERATION_CENTER_BASE_URL
+        
+    try:
+        created_at = params.get("created_at")
+        created_at = (datetime(created_at.split(",")[0]), datetime(created_at.split(",")[1]))
+    except:
+        created_at = None
+        
     payload: dict = {
         "status_uuid": params.get("status_uuid"),
         "status_name": params.get("status_name"),
         "short_id": params.get("short_id"),
         "rule_uuid": params.get("rule_uuid"),
         "rule_name": params.get("rule_name"),
-        "created_at": params.get("created_at"),
+        "created_at": created_at,
     }
 
     try:
